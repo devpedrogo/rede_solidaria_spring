77dd38c8582733a6aaa73b85c3a2112465097e87
Pedro Henrique Gouveia Cavalcante <pedrogouveia.dev@gmail.com>
2026-07-29 09:56:47 -0300

    fix: url frontend no webconfig

--- src/main/java/com/devpedrogo/redesolidaria/config/WebConfig.java
+++ src/main/java/com/devpedrogo/redesolidaria/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173", "https://seu-front-em-producao.vercel.app")
+                .allowedOrigins("http://localhost:5173", "https://rede-solidaria-frontend.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
